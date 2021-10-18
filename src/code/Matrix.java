16d56d0720a72a9cc4a311e50dac38ce94bc4e2c
Eslam Sabry <Eslam.221@gmail.com>
2021-10-18 06:38:26 +0200

    Project Initialization

---
+++ src/code/Matrix.java
@@ -0,0 +1,9 @@
+package code;
+
+public class Matrix extends SearchProblem {
+
+//	static String genGrid() {
+//	}
+//	static String solve(String grid, String strategy, boolean visualize) {
+//	}
+}
