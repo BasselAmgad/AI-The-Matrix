b95855d19c4bb03539ce7f94213be17d89c31913
seviared <bamgad7@Gmail.com>
2021-10-18 21:17:13 +0200

    Add SearchStrategy enum

--- src/code/Matrix.java
+++ src/code/Matrix.java
@@ -2,8 +2,41 @@ package code;
 
 public class Matrix extends SearchProblem {
 
-//	static String genGrid() {
-//	}
-//	static String solve(String grid, String strategy, boolean visualize) {
-//	}
+    //grid size
+    byte M, N;
+
+    int telephoneX, telephoneY;
+
+    int neoX, neoY;
+    int c; // <=4
+
+    // indexes of currently carried hostages ?
+    int[] currentC;
+
+    int n_hostages, n_pills, n_pads, n_agents;
+
+
+    int[] padsX, padsY;
+
+    int[] pillsX, pillsY;
+
+    int[] agentsX, agentsY;
+
+    int[] hostagesX, hostagesY;
+    int[] hostagesDamage;
+
+    int cost;
+
+    static String genGrid() {
+        // test
+        return "";
+    }
+
+    static String solve(String grid, String strategy, boolean visualize) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/code/SearchStrategy.java
@@ -0,0 +1,19 @@
+package code;
+
+/*
+ * An enum used to parse a search strategy string to.
+ */
+public enum SearchStrategy {
+    BF,
+    DF,
+    ID,
+    UC,
+    GR1,
+    GR2,
+    AS1,
+    AS2;
+
+    public static SearchStrategy parse(String searchStrategy) {
+        return SearchStrategy.valueOf(searchStrategy);
+    }
+}
